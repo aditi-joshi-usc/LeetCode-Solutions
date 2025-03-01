24370323dcf59d6fdfeb66282188df6a38610723
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-02-28 18:58:24 -0800

    Time: 55 ms, Memory: 31 MB - LeetHub

---
+++ 1272-147-1762-buildings-with-an-ocean-view/1272-147-1762-buildings-with-an-ocean-view.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        stack = []
+
+        for i in range(len(heights)):
+            while stack and heights[i] >= heights[stack[-1]]:
+                stack.pop()
+            stack.append(i)
+        return stack      
+
+
+        
