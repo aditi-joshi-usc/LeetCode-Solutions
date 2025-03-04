389972e005ba02c98fb2944f7306ac535c609eff
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-03 19:46:58 -0800

    Time: 1357 ms (17.62%), Space: 0B (100%) - LeetHub

--- 1399-page-recommendations/1399-page-recommendations.sql
+++ 1399-page-recommendations/1399-page-recommendations.sql
@@ -1,10 +1,18 @@
 # Write your MySQL query statement below
-with cte as(
-    select distinct user2_id as user_id from Friendship where user1_id = 1
-    UNION
-    select distinct user1_id as user_id from Friendship where user2_id = 1
-)
+-- with cte as(
+--     select distinct user2_id as user_id from Friendship where user1_id = 1
+--     UNION
+--     select distinct user1_id as user_id from Friendship where user2_id = 1
+-- )
+
+-- select distinct page_id as recommended_page from Likes 
+-- where user_id != 1 and user_id in (select user_id from cte) 
+-- and page_id not in(select page_id from Likes where user_id = 1);
+
 
 select distinct page_id as recommended_page from Likes 
-where user_id != 1 and user_id in (select user_id from cte) 
-and page_id not in(select page_id from Likes where user_id = 1);
+where (user_id in (select user2_id  from Friendship where user1_id = 1)
+or user_id in (select user1_id  from Friendship where user2_id = 1))
+AND page_id not in(select page_id from Likes where user_id = 1)
+
+;
