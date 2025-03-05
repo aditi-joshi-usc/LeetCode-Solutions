08bac475c48bed474f65b96529153426fc1b3ce5
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-05 11:52:42 -0800

    Time: 852 ms (63.43%), Space: 0B (100%) - LeetHub

---
+++ 1180-game-play-analysis-ii/1180-game-play-analysis-ii.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT player_id, device_id
+FROM
+(
+    SELECT player_id, device_id, RANK() OVER(PARTITION BY player_id ORDER BY event_date) AS date_rank FROM Activity 
+) AS t
+where date_rank = 1;
