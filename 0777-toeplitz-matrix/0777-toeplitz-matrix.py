dfc1ebb71d29c6ff30de6ea6f1c984d83cc1f315
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-05 09:53:08 -0800

    Time: 10 ms (6.53%), Space: 18 MB (8.1%) - LeetHub

---
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        isToeplitz = True
+        for i in range(m):
+            for j in range(n):
+                isToeplitz = self.isTP(matrix, i, j, m, n)
+                if not isToeplitz:
+                    return False
+        
+        return isToeplitz
+
+    def isTP(self,matrix, i, j, m,n):
+        if i>=m or j >= n:
+            return True
+        if i-1<0 or j-1< 0:
+            return True
+        
+        if matrix[i][j] == matrix[i-1][j-1]:
+            return self.isTP(matrix, i+1, j+1, m,n) and True
+        else:
+            return False
+
+
+
