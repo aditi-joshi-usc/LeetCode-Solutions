87233002e414eb3d874c721a36c8a7d229077e1c
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-04 16:33:18 -0800

    Time: 3 ms (91.14%), Space: 21 MB (89.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -8,11 +8,18 @@ class Solution:
                 track[num] = 1
 
 
-        q = []
-        for key, value in track.items():
-            q.append((value,key))
+        # q = []
+        # for key, value in track.items():
+        #     q.append((value,key))
         
-        res = heapq.nlargest(k, track, key = track.get)
-        # result =  sorted(track.keys(), key = lambda x: track[x], reverse = True)[:k]
-        # return result
-        return res
+        # res = heapq.nlargest(k, track, key = track.get)
+        # # result =  sorted(track.keys(), key = lambda x: track[x], reverse = True)[:k]
+        # # return result
+        # return res
+
+        heap = []
+        for key, value in track.items():
+            heapq.heappush(heap, (value, key))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return [item[1] for item in heap]
