4d5b72a1ae61bb23a6abe73d4782713f7fb218f7
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-05 09:41:58 -0800

    Time: 4 ms (49.42%), Space: 18.7 MB (9.8%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,24 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        track = defaultdict(int)
+        # track = defaultdict(int)
 
-        for i in range(len(numbers)):
-            if numbers[i] in track:
-                return [track[numbers[i]], i+1]
-            else:
-                track[target - numbers[i]] = i+1
+        # for i in range(len(numbers)):
+        #     if numbers[i] in track:
+        #         return [track[numbers[i]], i+1]
+        #     else:
+        #         track[target - numbers[i]] = i+1
+        # return [-1, -1]
+
+        low = 0
+        high = len(numbers) - 1
         
+        while low<high:
+            sumval = numbers[low] + numbers[high]
+
+            if sumval == target:
+                return [low+1, high+1]
+            elif sumval < target:
+                low+=1
+            else:
+                high -= 1
+        return [-1, -1]
