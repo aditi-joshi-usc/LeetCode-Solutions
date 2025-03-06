ddba0fe055f623d8f7b781031e394f45759f3751
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-06 13:52:26 -0800

    Time: 1920 ms (5.19%), Space: 0B (100%) - LeetHub

--- 1163-project-employees-iii/1163-project-employees-iii.sql
+++ 1163-project-employees-iii/1163-project-employees-iii.sql
@@ -3,7 +3,7 @@ WITH cte AS(SELECT
     p.project_id, p.employee_id,
     DENSE_RANK() OVER(PARTITION BY p.project_id ORDER BY experience_years DESC) AS rankid
 FROM Project AS p
-LEFT JOIN Employee AS e
+INNER JOIN Employee AS e
 ON p.employee_id = e.employee_id)
 
 SELECT project_id, employee_id
