93dea96b3dfad2e87fc73f804f429379704ab513
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-06 13:51:46 -0800

    Time: 871 ms (76.32%), Space: 0B (100%) - LeetHub

---
+++ 1163-project-employees-iii/1163-project-employees-iii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH cte AS(SELECT 
+    p.project_id, p.employee_id,
+    DENSE_RANK() OVER(PARTITION BY p.project_id ORDER BY experience_years DESC) AS rankid
+FROM Project AS p
+LEFT JOIN Employee AS e
+ON p.employee_id = e.employee_id)
+
+SELECT project_id, employee_id
+FROM cte WHERE rankid = 1;
