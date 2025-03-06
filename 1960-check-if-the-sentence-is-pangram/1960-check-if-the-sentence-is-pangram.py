b4e8a3a9ec17e92e8035374b0a85fcf1555678fd
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-06 09:29:34 -0800

    Time: 0 ms (100%), Space: 17.7 MB (50.59%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -1,12 +1,21 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        if len(sentence) < 26:
-            return False
-        track = defaultdict(int)
-        for letter in sentence:
-            track[letter] +=1
+        # if len(sentence) < 26:
+        #     return False
+        # track = defaultdict(int)
+        # for letter in sentence:
+        #     track[letter] +=1
         
-        if len(track) == 26:
-            return True
-        else:
-            return False
+        # if len(track) == 26:
+        #     return True
+        # else:
+        #     return False
+
+        seen = 0
+
+        for letter in sentence:
+            index = ord(letter) - ord('a')
+            curr_bit = 1 << index
+            seen |= curr_bit
+        
+        return seen+1 == 1 << 26
