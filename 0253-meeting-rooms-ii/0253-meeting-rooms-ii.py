ffd0818cfa4f249d45240c079d88de6c0dfdb29e
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-17 10:18:40 -0700

    Time: 2 ms (90.67%), Space: 20 MB (49.15%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,15 +1,26 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        track = defaultdict(int)
+        # track = defaultdict(int)
 
+        # for start, end in intervals:
+        #     track[start]+=1
+        #     track[end]-=1
+        
+        # rooms = 0
+        # k = 0
+        
+        # for key in sorted(track):
+        #     rooms+=track[key]
+        #     k = max(k, rooms)
+        # return k
+        intervals.sort(key =  lambda x : x[0])
+        heap = []
+        minroom = 1
         for start, end in intervals:
-            track[start]+=1
-            track[end]-=1
-        
-        rooms = 0
-        k = 0
-        
-        for key in sorted(track):
-            rooms+=track[key]
-            k = max(k, rooms)
-        return k
+            
+            if heap and heap[0] <= start:
+                heapq.heappop(heap)
+            elif heap and heap[0] > start:
+                minroom+=1
+            heapq.heappush(heap, end)
+        return minroom
