14e653d06dae11a64c674c66aad6fa753c1fe081
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-17 14:26:37 -0700

    Time: 185 ms (42.92%), Space: 18.7 MB (74.43%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,24 +1,37 @@
 class MyCalendar:
 
+    # def __init__(self):
+    #     self.track = defaultdict(int)
+
+    # def book(self, startTime: int, endTime: int) -> bool:
+        
+    #     self.track[startTime] +=1
+    #     self.track[endTime] -=1          
+    #     ans = True
+    #     sumval=0
+    #     for key in sorted(self.track):
+    #         sumval += self.track[key]  
+    #         if sumval > 1:
+    #             ans = False
+    #             break
+    #     if not ans:
+    #         self.track[startTime] -=1
+    #         self.track[endTime] +=1  
+
+    #     return ans
+
     def __init__(self):
-        self.track = defaultdict(int)
+
+        self.events = []
 
     def book(self, startTime: int, endTime: int) -> bool:
         
-        self.track[startTime] +=1
-        self.track[endTime] -=1          
-        ans = True
-        sumval=0
-        for key in sorted(self.track):
-            sumval += self.track[key]  
-            if sumval > 1:
-                ans = False
-                break
-        if not ans:
-            self.track[startTime] -=1
-            self.track[endTime] +=1  
-
-        return ans
+        for start, end in self.events:
+            if startTime <end and start< endTime:
+                return False
+        self.events.append([startTime, endTime])
+        return True
+       
 
 
 # Your MyCalendar object will be instantiated and called as such:
