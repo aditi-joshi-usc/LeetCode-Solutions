9e264e3a78d96a4b4c46c712aa21c9df9adb7847
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-17 10:48:30 -0700

    Time: 1 ms (86.53%), Space: 19.8 MB (58.81%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,16 +1,30 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        track = defaultdict(int)
+        # track = defaultdict(int)
 
-        for start, end in intervals:
-            track[start]+=1
-            track[end]-=1
+        # for start, end in intervals:
+        #     track[start]+=1
+        #     track[end]-=1
         
-        meeting_count = 0
+        # meeting_count = 0
 
-        for key in sorted(track):
-            meeting_count+=track[key]
+        # for key in sorted(track):
+        #     meeting_count+=track[key]
 
-            if meeting_count > 1:
+        #     if meeting_count > 1:
+        #         return False
+        # return True
+
+
+        intervals.sort(key = lambda x:x[0])
+
+        minheap = []
+        prev_s = 0
+        prev_e = 0
+        for start, end in intervals:
+            if prev_s<= start < prev_e or prev_s< end<=prev_e:
                 return False
+            prev_s, prev_e = start, end
         return True
+
+           
