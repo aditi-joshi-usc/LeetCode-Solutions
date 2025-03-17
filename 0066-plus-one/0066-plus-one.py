3a3036f38e8fb4afe69af7cc177a8679fa0bf84a
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-17 15:05:30 -0700

    Time: 0 ms (100%), Space: 17.7 MB (80.1%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,15 +1,24 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
-        carry = 1
+        # carry = 1
+
+        # for i in range(len(digits) -1, -1, -1):
+        #     sumval = 0
+        #     sumval = digits[i] + carry
+            
+        #     digits[i] = sumval % 10
+        #     carry = sumval //10
+        # if carry:
+        #     digits = [carry] + digits
+        # return digits 
 
         for i in range(len(digits) -1, -1, -1):
-            sumval = 0
-            sumval = digits[i] + carry
-            
-            digits[i] = sumval % 10
-            carry = sumval //10
-        if carry:
-            digits = [carry] + digits
-        return digits 
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        
+        return [1] + digits 
+
             
