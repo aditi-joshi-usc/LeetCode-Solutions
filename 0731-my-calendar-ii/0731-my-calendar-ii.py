6b7825a8f47534876306065d7f8d4ae0089880dc
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-17 14:54:58 -0700

    Time: 873 ms (55.48%), Space: 18.7 MB (65.96%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,28 +1,50 @@
 class MyCalendarTwo:
 
-    def __init__(self):
-        self.track = defaultdict(int)
+    # def __init__(self):
+    #     self.track = defaultdict(int)
         
 
+    # def book(self, startTime: int, endTime: int) -> bool:
+    #     self.track[startTime] +=1
+    #     self.track[endTime] -=1
+
+    #     ans = True
+    #     sumval = 0
+    #     for key in sorted(self.track):
+    #         sumval+= self.track[key]
+    #         if sumval>2:
+    #             ans = False
+    #             break
+    #     if not ans:
+    #         self.track[startTime] -=1
+    #         self.track[endTime] +=1
+    #     return ans
+
+
+
+
+    def __init__(self):
+        
+        self.single_bookings = []
+        self.double_bookings = []
+
+
     def book(self, startTime: int, endTime: int) -> bool:
-        self.track[startTime] +=1
-        self.track[endTime] -=1
+        
+        for dstart, dend in self.double_bookings:
 
-        ans = True
-        sumval = 0
-        for key in sorted(self.track):
-            sumval+= self.track[key]
-            if sumval>2:
-                ans = False
-                break
-        if not ans:
-            self.track[startTime] -=1
-            self.track[endTime] +=1
-        return ans
+            if max(dstart, startTime) < min(dend, endTime):
+                return False
+        
 
+        for sstart, send in self.single_bookings:
 
-
-
+            if max(sstart, startTime) < min(send, endTime):
+                self.double_bookings.append([max(sstart, startTime), min(send, endTime)])
+        
+        self.single_bookings.append([startTime, endTime])
+        return True
+        
 
 # Your MyCalendarTwo object will be instantiated and called as such:
 # obj = MyCalendarTwo()
