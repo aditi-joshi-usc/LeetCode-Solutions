9a224f7d844021553019d01aa1422833130dd7e7
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-18 12:52:47 -0700

    Time: 0 ms (100%), Space: 17.7 MB (82.17%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,25 +1,50 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
+        # res = []
+        # letters = ['a', 'b', 'c']
+        # def dfs(s):
+        #     if len(s) == n:
+        #         res.append(s.copy())
+        #     if len(res) == k:
+        #         return 
+        #     if len(s) > n:
+        #         return
+
+        #     for letter in letters:
+        #         if len(s) > 0 and letter == s[-1]:
+        #             continue
+        #         s.append(letter)
+        #         dfs(s)
+        #         s.pop()
+        # dfs([])
+
+        # if len(res) >= k:
+        #     return ''.join(res[k-1])
+        # else:
+        #     return ''
+
         res = []
-        letters = ['a', 'b', 'c']
-        def dfs(s):
-            if len(s) == n:
-                res.append(s.copy())
-            if len(res) == k:
-                return 
-            if len(s) > n:
-                return
 
-            for letter in letters:
-                if len(s) > 0 and letter == s[-1]:
-                    continue
-                s.append(letter)
-                dfs(s)
-                s.pop()
-        dfs([])
+        total_happy = 3 * (2**(n-1))
+         
+        
+        
+        left =1
+        right = total_happy
 
-        if len(res) >= k:
-            return ''.join(res[k-1])
-        else:
-            return ''
-     
+        choices = 'abc'
+
+        for i in range(n):
+            curr = left
+            partition_size =  (right - left +1 )//len(choices)
+
+            for ch in choices:
+                
+                if k in range(curr, curr+partition_size):
+                    res.append(ch)
+                    left = curr
+                    right = curr + partition_size - 1
+                    choices = 'abc'.replace(ch, '')
+                    break
+                curr += partition_size
+        return ''.join(res)
