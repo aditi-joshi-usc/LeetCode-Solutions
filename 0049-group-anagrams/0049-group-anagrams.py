e489dbfbfc5478033f80924623871c08e3581ea7
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-18 16:31:55 -0700

    Time: 20 ms (27.29%), Space: 22.6 MB (32.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        track = {}
+        # track = {}
+
+        # for s in strs:
+        #     slist = ''.join(sorted(s))
+        #     if slist in track:
+        #         track[slist].append(s)
+        #     else:
+        #         track[slist] = [s]
+        # return list(track.values())
+
+        track = defaultdict(list)
 
         for s in strs:
-            slist = ''.join(sorted(s))
-            if slist in track:
-                track[slist].append(s)
-            else:
-                track[slist] = [s]
-        return list(track.values())
 
+            count = [0]*26
+            for c in s:
+                count[ord(c) - ord('a')]+=1
+            track[tuple(count)].append(s)
+        return list(track.values()) 
