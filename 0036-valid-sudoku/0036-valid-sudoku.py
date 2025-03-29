a5c705b29413c00b43c3bedc25d43485a0d1512b
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-28 22:34:28 -0700

    Time: 3 ms (83.35%), Space: 17.9 MB (25%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,19 +2,19 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = defaultdict(list)
         cols = defaultdict(list)
-        grid = [[[] for i in range(3)] for _ in range(3)]
+        grid = defaultdict(list)
 
         for r in range(9):
             for c in range(9):
                 num = board[r][c]
                 if num == '.':
                     continue
-                if num in rows[r] or num in cols[c] or num in grid[r//3][c//3]:
+                if num in rows[r] or num in cols[c] or num in grid[(r//3,c//3)]:
                     return False
                 else:
                     rows[r].append(num)
                     cols[c].append(num)
-                    grid[r//3][c//3].append(num)
+                    grid[(r//3,c//3)].append(num)
         return True  
 
 
