fd9b8f1ddd7234c6f8c00a4ef62943b0d230bcfa
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-30 20:17:30 -0700

    Time: 0 ms (100%), Space: 18.6 MB (39.08%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,3 +1,25 @@
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+#         ROWS = len(matrix)
+#         COLS = len(matrix[0])
+
+#         row_list = set()
+#         col_list = set()
+
+#         for r in range(ROWS):
+#             for c in range(COLS):
+#                 if matrix[r][c] == 0:
+#                     row_list.add(r)
+#                     col_list.add(c)
+#         for r in row_list:
+#             matrix[r] = [0 for _ in range(COLS)]
+#         for c in col_list:
+#             for r in range(ROWS):
+#                 matrix[r][c] = 0
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
@@ -5,17 +27,23 @@ class Solution:
         """
         ROWS = len(matrix)
         COLS = len(matrix[0])
-
-        row_list = set()
-        col_list = set()
-
+        is_col = False
         for r in range(ROWS):
-            for c in range(COLS):
+            if matrix[r][0] == 0:
+                is_col = True
+            for c in range(1, COLS):
                 if matrix[r][c] == 0:
-                    row_list.add(r)
-                    col_list.add(c)
-        for r in row_list:
-            matrix[r] = [0 for _ in range(COLS)]
-        for c in col_list:
+                    matrix[0][c] = 0
+                    matrix[r][0] = 0
+
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] ==0 or matrix[r][0] ==0:
+                    matrix[r][c] = 0
+        if matrix[0][0] == 0:
+            for c in range(COLS):
+                matrix[0][c] = 0
+            
+        if is_col:
             for r in range(ROWS):
-                matrix[r][c] = 0
+                matrix[r][0] = 0
