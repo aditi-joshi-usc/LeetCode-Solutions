e64de4188e6f1e7a064f659ad830b0aa67a3f840
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-31 18:16:39 -0700

    Time: 10 ms (75.18%), Space: 17.8 MB (86.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
         track1 = defaultdict(int)
         track2 = defaultdict(int)
 
