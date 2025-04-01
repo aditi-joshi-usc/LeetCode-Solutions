204b6723bac7edae5cd0fb92ca2d26781cf83bbe
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-03-31 18:17:48 -0700

    Time: 2959 ms (5.16%), Space: 17.9 MB (64.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,11 +3,15 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        track1 = defaultdict(int)
-        track2 = defaultdict(int)
+        # track1 = defaultdict(int)
+        # track2 = defaultdict(int)
 
-        for w in s:
-            track1[w] +=1
-        for w in t:
-            track2[w] +=1
-        return track1 == track2
+        # for w in s:
+        #     track1[w] +=1
+        # for w in t:
+        #     track2[w] +=1
+        # return track1 == track2
+        for i in s:
+            if s.count(i) != t.count(i):
+                return False
+        return True
