d25a12229328f7453ed6f422c52f4c2c8990f866
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-07 15:44:47 -0700

    Time: 3 ms (30.05%), Space: 17.6 MB (93.32%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -5,29 +5,47 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        visited = set()
+        # visited = set()
+
+        # dummy = ListNode(0)
+        # dummy.next = head
+        # prev= None
+        # curr = head
+        # repeat = set()
+
+        # while curr:
+        #     if curr.val in visited:
+        #         prev.next= curr.next
+        #         repeat.add(curr.val)
+        #     else:
+        #         visited.add(curr.val)
+        #         prev = curr
+        #     curr = curr.next
+        
+        # curr = head
+        # prev = dummy
+        # while curr:
+        #     if curr.val in repeat:
+        #         prev.next= curr.next 
+        #     else:
+        #         prev = curr
+        #     curr = curr.next
+        # return dummy.next
 
         dummy = ListNode(0)
-        dummy.next = head
-        prev= None
+        prev = dummy
         curr = head
-        repeat = set()
+        dummy.next = curr
 
         while curr:
-            if curr.val in visited:
-                prev.next= curr.next
-                repeat.add(curr.val)
+            isduplicate = False
+            while curr and curr.next and curr.val == curr.next.val:
+                curr = curr.next
+                isduplicate = True
+            if isduplicate:
+                prev.next = curr.next
+                curr = curr.next
             else:
-                visited.add(curr.val)
-                prev = curr
-            curr = curr.next
-        
-        curr = head
-        prev = dummy
-        while curr:
-            if curr.val in repeat:
-                prev.next= curr.next 
-            else:
-                prev = curr
-            curr = curr.next
+                curr, prev = curr.next, curr
         return dummy.next
+
