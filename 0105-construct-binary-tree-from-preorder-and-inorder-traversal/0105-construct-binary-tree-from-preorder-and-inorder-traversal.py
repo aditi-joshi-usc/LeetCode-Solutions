6a320d8b9d3ee028a8eabd15141c96d980727d46
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-07 17:50:53 -0700

    Time: 5 ms (70.88%), Space: 19.2 MB (74.61%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -11,23 +11,37 @@ class Solution:
       
         
         
-        def buildtree(pre, inor):
-            if not pre or not inor:
-                return None
+        # def buildtree(pre, inor):
+        #     if not pre or not inor:
+        #         return None
             
-            root = TreeNode(pre[0])
-            mid = inor.index(pre[0])
+        #     root = TreeNode(pre[0])
+        #     mid = inor.index(pre[0])
 
-            leftIn = inor[:mid]
-            rightIn = inor[mid+1:]
-            leftPre = pre[1:mid+1]
-            rightPre = pre[mid+1:]
+        #     leftIn = inor[:mid]
+        #     rightIn = inor[mid+1:]
+        #     leftPre = pre[1:mid+1]
+        #     rightPre = pre[mid+1:]
 
-            root.left = buildtree(leftPre, leftIn)
-            root.right = buildtree(rightPre, rightIn)
+        #     root.left = buildtree(leftPre, leftIn)
+        #     root.right = buildtree(rightPre, rightIn)
 
-            return root            
+        #     return root            
 
         
-        return buildtree(preorder, inorder)
-        
+        # return buildtree(preorder, inorder)
+
+        track = {}
+
+        for index, val in enumerate(inorder):
+            track[val] = index
+        n = len(preorder)
+        def buildtree(prepos, left, right):
+            if prepos >=n or right>=n or left>=n or left > right:
+                return None
+            inpos = track[preorder[prepos]]
+            root = TreeNode(preorder[prepos])
+            root.left = buildtree(prepos+1, left, inpos-1)
+            root.right = buildtree(prepos+ inpos-left +1, inpos+1, right) 
+            return root
+        return buildtree(0, 0, n-1)
