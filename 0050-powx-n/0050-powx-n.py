6cec5ed145b01032847e2fc5435a5ba99abdd5db
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-09 18:52:41 -0700

    Time: 0 ms (100%), Space: 18.1 MB (11.49%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -3,12 +3,9 @@ class Solution:
 
 
         def findpow(x,n):
-            if n<=0:
+            if n==0:
                 return 1
-            if n == 1:
-                return x
-            if n ==2:
-                return x*x
+            
             half=findpow(x,n//2)
             if n%2 == 0:
                 return half * half
