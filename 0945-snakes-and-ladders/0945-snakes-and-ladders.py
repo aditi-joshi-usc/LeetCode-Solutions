fee8736a9caf7bff831dd66d1861e2d48f2f8b10
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-12 16:08:41 -0700

    Time: 19 ms (70.99%), Space: 18 MB (37.74%) - LeetHub

---
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -0,0 +1,32 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        length = len(board)
+        board.reverse()
+
+        def numPos(square):
+            square -=1 
+            r = square//length
+            c  = square % length
+            if r % 2 !=0:
+                c = length -1 - c
+           
+            return [r,c]
+        
+        q = deque()
+        q.append((1, 0)) #[square, moves]
+        visit = set()
+        while q:
+            
+            square, moves = q.popleft()
+
+            for i in range(1,7):
+                nextsquare = square+i
+                r,c = numPos(nextsquare)
+                if board[r][c] != -1:
+                    nextsquare = board[r][c]
+                if nextsquare == length * length:
+                    return moves+1
+                if nextsquare not in visit:
+                    visit.add(nextsquare)
+                    q.append([nextsquare, moves+1])
+        return -1
