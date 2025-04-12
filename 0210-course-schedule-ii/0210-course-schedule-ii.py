e944735813f8dd838baa6d544dd851f0a859839c
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-12 14:42:54 -0700

    Time: 31 ms (6.04%), Space: 19.7 MB (25.22%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,51 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        if not prerequisites:
+            return [n for n in range(numCourses)]
+        
+        visited = 2
+        unvisited = 0
+        visiting = 1
+
+        levels = [0 for _ in range(numCourses)]
+
+        track = defaultdict(list)
+        for course, prereq in prerequisites:
+            track[course].append(prereq)
+        
+        res = []
+
+        def traverse(course):
+            if course>=numCourses:
+                return 
+            level  = levels[course]
+            if level == 2:
+                return 
+            if level == 1:
+                return 
+            levels[course] = 1
+            cantake = True
+            for pre in track[course]:
+                if levels[pre] == 2:
+                    continue
+                if levels[pre] == 0:
+                    traverse(pre)
+                else:
+                    cantake = False
+                    break
+            if cantake:
+                levels[course] = 2
+                if course not in res:
+                    res.append(course)
+            
+        for i in range(numCourses):
+            traverse(i)
+        
+        if 1 in levels:
+            return []
+        else:
+            return res
+        
+                
+            
+
