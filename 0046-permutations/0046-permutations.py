e10bc35c6dc8c86911d2c368f7b9fb2f7d6a1078
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-13 12:24:43 -0700

    Time: 7 ms (3.42%), Space: 18.1 MB (32.72%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+
+        res = []
+        nlen = len(nums)
+
+        def perm(subarray):
+            if len(subarray) == nlen and subarray not in res:
+                res.append(subarray.copy())
+            
+            for num in nums:
+                if num not in subarray:
+                    subarray.append(num)
+                    perm(subarray)
+                    subarray.pop()
+        perm([])
+        return res
