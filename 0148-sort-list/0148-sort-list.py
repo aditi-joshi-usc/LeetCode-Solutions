10b090cc06d4ce8606095bc91c38a571aabf06df
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-13 18:01:46 -0700

    Time: 205 ms (27.75%), Space: 32.9 MB (68.88%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -6,21 +6,58 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        minheap = []
+        # minheap = []
+        # node  =  head
 
-
-        node  =  head
-
-        while node:
+        # while node:
             
-            heapq.heappush(minheap,node.val)
-            node = node.next
+        #     heapq.heappush(minheap,node.val)
+        #     node = node.next
         
+        # dummy = ListNode()
+        # head = dummy
+        # while minheap:
+        #     val = heapq.heappop(minheap)
+        #     head.next = ListNode(val)
+        #     head = head.next
+        
+        # return dummy.next
+
+        if not head or not head.next:
+            return head
+        
+        slow  = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        mid = slow.next
+        slow.next = None
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        return self.merge(left, right)
+    
+
+    def merge(self, left, right):
         dummy = ListNode()
-        head = dummy
-        while minheap:
-            val = heapq.heappop(minheap)
-            head.next = ListNode(val)
-            head = head.next
-        
+        node = dummy
+        while left and right:
+
+            if left.val > right.val:
+                node.next = right
+                right = right.next
+            else:
+                node.next = left
+                left = left.next
+            node = node.next
+            
+            if left:
+                node.next = left
+            if right:
+                node.next = right
         return dummy.next
+
+
+
