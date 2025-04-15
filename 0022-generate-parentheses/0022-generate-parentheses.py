4c693e24aa9ef80cdb0293fbdc27d161a0dd9d28
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-14 17:17:18 -0700

    Time: 9 ms (5.61%), Space: 18.2 MB (26.43%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+
+        
+        llen = 2*n
+        res = []
+        def backtrack(open, close, subparan):
+            if len(subparan) == llen and open == close:
+                res.append(subparan)
+                return
+            if len(subparan) >llen:
+                return
+            
+
+            if open < llen:
+                backtrack(open+1, close, subparan+'(')
+            if close < open:
+                backtrack(open, close+1, subparan+')')
+
+    
+            
+        backtrack(0,0, '')
+        return res
+            
