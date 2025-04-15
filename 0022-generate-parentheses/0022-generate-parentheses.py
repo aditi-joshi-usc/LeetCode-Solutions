5ff8d83372233ddb5e80b9f52ce50f0b32674397
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-14 17:20:12 -0700

    Time: 3 ms (30.85%), Space: 17.8 MB (92.52%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -9,11 +9,11 @@ class Solution:
             if open == n and open == close:
                 res.append(subparan)
                 return
-            if len(subparan) >llen:
+            if open > n or close > n:
                 return
             
 
-            if open < llen:
+            if open < n:
                 backtrack(open+1, close, subparan+'(')
             if close < open:
                 backtrack(open, close+1, subparan+')')
