7c857eb2febb87ef7dd8e48474f267cc9042a8b5
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-14 17:20:51 -0700

    Time: 3 ms (30.85%), Space: 18.1 MB (26.43%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -8,15 +8,11 @@ class Solution:
                 return
             if open > n or close > n:
                 return
-            
-
             if open < n:
                 backtrack(open+1, close, subparan+'(')
             if close < open:
                 backtrack(open, close+1, subparan+')')
-
-    
-            
+        
         backtrack(0,0, '')
         return res
             
