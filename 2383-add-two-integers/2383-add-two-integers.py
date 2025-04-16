771c12c1c16b576307895920883cc3296ceecd8e
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-16 16:35:50 -0700

    Time: 0 ms (100%), Space: 17.7 MB (42.46%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.py
+++ 2383-add-two-integers/2383-add-two-integers.py
@@ -1,3 +1,14 @@
 class Solution:
     def sum(self, num1: int, num2: int) -> int:
-        return num1+num2
+        left = -200
+        right = 200
+
+        while left<right:
+            mid = (left + right)//2
+            if mid == num1+num2:
+                return mid
+            elif mid> num1+num2:
+                right = mid-1
+            else:
+                left = mid + 1
+        return left
