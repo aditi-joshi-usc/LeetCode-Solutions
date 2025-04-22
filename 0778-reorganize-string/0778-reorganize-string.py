a2abc317e3def619af01e9e3afc5124f4c1e582e
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-22 13:33:54 -0700

    Time: 3 ms (48.25%), Space: 17.8 MB (47.93%) - LeetHub

---
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -0,0 +1,37 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        pq = []
+
+        track = {}
+        for schar in s:
+            if schar in track:
+                track[schar] +=1
+            else:
+                track[schar] =1
+        
+        for key, value in track.items():
+            pq.append((-value, key))
+        
+        heapq.heapify(pq)
+
+        res = []
+
+        while pq:
+            count, char = heapq.heappop(pq)
+            if res and res[-1] == char:
+                if pq:
+                    second_cnt, second_char = heapq.heappop(pq)
+                    res.append(second_char)
+                    second_cnt+=1
+                    if second_cnt!=0:
+                        heapq.heappush(pq, (second_cnt, second_char))
+                    heapq.heappush(pq, (count,char))
+                else:
+                    return ""
+                
+            else:
+                res.append(char)
+                count+=1
+                if count!=0:
+                    heapq.heappush(pq, (count, char))
+        return ''.join(res)
