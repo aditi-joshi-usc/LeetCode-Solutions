5a66bf22aa939217a61f3e63c5f066a3076bae13
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-22 17:53:25 -0700

    Time: 434 ms (97.18%), Space: 17.8 MB (87.17%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,7 +3,7 @@ class Solution:
         directions = [(-1,0), (1,0), (0,1), (0,-1)]
         m = len(board)
         n = len(board[0])
-        visited = [[False for _ in range(n)] for i in range(m)]
+        # visited = [[False for _ in range(n)] for i in range(m)]
         
 
         word_cnt = Counter(word)
@@ -17,13 +17,13 @@ class Solution:
             if index == len(word)-1:
                 return True
             
-            visited[r][c] = True
+            board[r][c] = "#"
             for dr, dc in directions:
                 nr, nc = r+dr, c+dc
-                if 0<=nr<m and 0<=nc<n and visited[nr][nc] == False and  board[nr][nc] == word[index+1]:
+                if 0<=nr<m and 0<=nc<n and board[nr][nc] == word[index+1]:
                     if backtrack(nr, nc, index+1):
                         return True
-            visited[r][c] = False
+            board[r][c] = word[index]
             return False
                 
 
