c16dccc101e8239aa3c2dd91b72dad1938155b20
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-23 23:32:10 -0700

    Time: 3 ms (16.75%), Space: 18.2 MB (25.94%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,24 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        curr_color = image[sr][sc]
+
+        directions = [(1,0), (-1, 0), (0,1), (0,-1)]
+        m = len(image)
+        n = len(image[0])
+        visited = set()
+        def dfs(r,c):
+            if r<0 or r>=m or c<0 or c>=n:
+                return
+            if (r,c) in visited:
+                return
+
+            if image[r][c] !=curr_color:
+                return
+        
+            image[r][c] = color
+            visited.add((r,c))
+            for dr, dc in directions:
+                dfs(dr+r, dc+c)
+        
+        dfs(sr,sc)
+        return image
