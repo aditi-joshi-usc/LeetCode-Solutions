82c2428011a438be281e63a71d3dfa57c4b48cd5
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-29 16:47:33 -0700

    Time: 15 ms (36.85%), Space: 17.9 MB (85.71%) - LeetHub

---
+++ 3825-apply-substitutions/3825-apply-substitutions.py
@@ -0,0 +1,33 @@
+class Solution:
+    def applySubstitutions(self, replacements: List[List[str]], text: str) -> str:
+        track = {}
+
+        for key, val in replacements:
+            track[key] = val
+        
+        i = 0
+
+        if not text:
+            return text
+        
+        
+        return self.replacesubstring(text, track)
+
+
+    def replacesubstring(self, text, track):
+        res = []
+        text_len = len(text)
+        i=0
+        while i < text_len:
+
+            if text[i] == '%':
+                j = i+1
+                while j < text_len and text[j] != '%':
+                    j+=1
+                res.append(self.replacesubstring(track[text[i+1:j]], track))
+                i = j+1
+            else:
+                res.append(text[i])
+                i+=1
+        
+        return ''.join(res)
