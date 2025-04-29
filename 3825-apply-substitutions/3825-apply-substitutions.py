f292e016a1b0b8a0c9280438f15303aa0a79176d
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-04-29 16:51:40 -0700

    Time: 7 ms (78.33%), Space: 18.1 MB (60.06%) - LeetHub

--- 3825-apply-substitutions/3825-apply-substitutions.py
+++ 3825-apply-substitutions/3825-apply-substitutions.py
@@ -1,9 +1,9 @@
 class Solution:
     def applySubstitutions(self, replacements: List[List[str]], text: str) -> str:
-        self.track = {}
+        track = {}
 
         for key, val in replacements:
-            self.track[key] = val
+            track[key] = val
         
         i = 0
 
@@ -11,10 +11,10 @@ class Solution:
             return text
         
         
-        return self.replacesubstring(text)
+        return self.replacesubstring(text, track)
 
 
-    def replacesubstring(self, text):
+    def replacesubstring(self, text, track):
         res = []
         if '%' not in text:
             return text
@@ -26,7 +26,7 @@ class Solution:
                 j = i+1
                 while j < text_len and text[j] != '%':
                     j+=1
-                res.append(self.replacesubstring(self.track[text[i+1:j]]))
+                res.append(self.replacesubstring(track[text[i+1:j]], track))
                 i = j+1
             else:
                 res.append(text[i])
