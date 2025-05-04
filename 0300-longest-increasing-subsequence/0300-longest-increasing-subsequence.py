0a4cfe68278be8793783f3891705fcb3aa651aa7
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-05-03 17:53:15 -0700

    Time: 7 ms (84.45%), Space: 18.1 MB (37.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -7,13 +7,35 @@ class Solution:
         #         if nums[curr] > nums[prev]:
         #             dp[curr] = max(dp[prev]+1, dp[curr])
         # return max(dp)
+        #sub = []
+
+        # for num in nums:
+        #     i = bisect_left(sub, num)
+
+        #     if i == len(sub):
+        #         sub.append(num)
+        #     else: 
+        #         sub[i] = num
+        # return len(sub)
+
         sub = []
 
-        for num in nums:
-            i = bisect_left(sub, num)
 
-            if i == len(sub):
+        for num in nums:
+
+            left = 0
+            right = len(sub)
+
+            while left < right:
+                mid = (left+right)//2
+
+                if sub[mid] < num:
+                    left = mid + 1
+                else:
+                    right = mid
+            
+            if left == len(sub):
                 sub.append(num)
-            else: 
-                sub[i] = num
+            else:
+                sub[left] = num
         return len(sub)
