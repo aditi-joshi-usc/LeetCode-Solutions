64089ebe74d97863e4e92172712436bb1cb51b16
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-05-09 18:43:17 -0700

    Time: 123 ms (30.3%), Space: 19.1 MB (9.09%) - LeetHub

---
+++ 1526-html-entity-parser/1526-html-entity-parser.py
@@ -0,0 +1,38 @@
+class Solution:
+    def entityParser(self, text: str) -> str:
+        track = {
+            '&quot;': '\"',
+            '&apos;': '\'',
+            '&amp;': '&',
+            '&gt;': '>',
+            '&lt;': '<',
+            '&frasl;': '/'
+        }
+
+        # start traversing through entire string  and look for &
+
+
+        res = []
+        i=0
+        while i < len(text):
+
+            if text[i] == '&':
+                j = min(i+7, len(text))
+
+                while j>i:
+                    if text[i:j] in track:
+                        res.append(track[text[i:j]])
+                        break
+                    j-=1
+                if j==i:
+                    res.append(text[i])
+                    i+=1
+                else:
+                    i = j
+                
+                
+            else:
+                res.append(text[i])
+                i+=1
+        return ''.join(res)
+
