265fc5de9671133cd1795298fd2760a235a17737
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-05-09 18:45:54 -0700

    Time: 19 ms (76.97%), Space: 18.7 MB (47.88%) - LeetHub

--- 1526-html-entity-parser/1526-html-entity-parser.py
+++ 1526-html-entity-parser/1526-html-entity-parser.py
@@ -12,27 +12,34 @@ class Solution:
         # start traversing through entire string  and look for &
 
 
-        res = []
-        i=0
-        while i < len(text):
+        # res = []
+        # i=0
+        # while i < len(text):
 
-            if text[i] == '&':
-                j = min(i+7, len(text))
+        #     if text[i] == '&':
+        #         j = min(i+7, len(text))
 
-                while j>i:
-                    if text[i:j] in track:
-                        res.append(track[text[i:j]])
-                        break
-                    j-=1
-                if j==i:
-                    res.append(text[i])
-                    i+=1
-                else:
-                    i = j
+        #         while j>i:
+        #             if text[i:j] in track:
+        #                 res.append(track[text[i:j]])
+        #                 break
+        #             j-=1
+        #         if j==i:
+        #             res.append(text[i])
+        #             i+=1
+        #         else:
+        #             i = j
                 
                 
-            else:
-                res.append(text[i])
-                i+=1
-        return ''.join(res)
+        #     else:
+        #         res.append(text[i])
+        #         i+=1
+        # return ''.join(res)
 
+        res = text
+
+        for key in track:
+            if key in text:
+                res = res.replace(key, track[key])
+
+        return res
