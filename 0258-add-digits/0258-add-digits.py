4a95f4dc207abd94a1ae17d0f91d41a3f11cb9ab
Aditi Joshi <156352197+aditi-joshi-usc@users.noreply.github.com>
2025-05-14 15:52:27 -0700

    Time: 3 ms (17.49%), Space: 17.8 MB (37.6%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,42 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        '''        
+
+                38
+                3 + 8 = 11
+                1+1 = 12
+
+                456
+                15
+                6
+
+
+                12334
+                13
+                68768
+                35
+                8
+
+
+                45
+                9
+        '''
+
+        if num ==0:
+            return num
+        numlist = []
+        temp = num
+
+        while temp!=0:
+            numlist.append(temp%10)
+            temp = temp//10
+        if len(numlist) == 1:
+            return num
+        while len(numlist) > 1:
+            temp = sum(numlist)
+            numlist = []
+            while temp!=0:
+                numlist.append(temp%10)
+                temp = temp//10
+
+        return numlist[0]
